c3055be37acd4241d63ebeb3699d6cebfcc6144d
yuliana-alves <yulianaalves0101@gmail.com>
2025-05-26 21:34:22 -0300

    intento de header

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
+const Header = () => {
+  return (
+    <div className="headerC">
+      <header className="header">
+        <h1 className="titulo">Mi Sitio</h1>
+        <nav className="navbar">
+          <a href="#inicio" className="link">
+            Inicio
+          </a>
+          <a href="#sobre-mi" className="link">
+            Sobre mí
+          </a>
+          <a href="#contacto" className="link">
+            Contacto
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
 
-
-export default App
+export default Header;
