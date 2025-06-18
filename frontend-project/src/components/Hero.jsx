6b12ad1460adb1309d587f9b56c5ce96b6acc3d5
yuliana-alves <yulianaalves0101@gmail.com>
2025-06-17 22:33:11 -0300

    cambios en el app
    mejorando el front

--- frontend-project/src/components/Hero.jsx
+++ frontend-project/src/components/Hero.jsx
@@ -1,16 +1,39 @@
+import React from "react";
 import "./Hero.css";
 
-function Hero() {
+const Hero = () => {
+  const handleScroll = () => {
+    const currentPosition = window.scrollY;
+    const targetPosition = currentPosition + 400;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="hero-container">
-      <div className="logo-hero">
-        <span>MAISON PET</span>
+      <div className="hero-content">
+        <p className="welcome-text">BIENVENIDO A</p>
+        <h1 className="main-title">MAISON PET</h1>
+        <p className="description">
+          En Maison Pet, creemos que todos los animales merecen amor, cuidado y un hogar donde se sientan seguros.
+        </p>
+        <button className="button-banner" onClick={handleScroll}>
+          Ver más
+        </button>
       </div>
+
       <div className="circle-image">
-        <img src="https://media.istockphoto.com/id/184365735/es/foto/comida-para-perro.jpg?s=612x612&w=0&k=20&c=YJ82RFRyt7WOJbTPWKO3PzASai_eRVmo80iRc5WrVmE=" alt="Pet" />
+        <img
+          src="https://img.freepik.com/fotos-premium/tazon-comida-perros-que-tiene-comida-perros-el_854727-122783.jpg"
+          alt="Elegant Cravings"
+          className="hero-image"
+        />
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
